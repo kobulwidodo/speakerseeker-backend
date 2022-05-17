fde4c048a725e52133bf347820833f95174da8e3
kobulwidodo <fadhilhan01@gmail.com>
2022-05-17 20:49:52 +0700

    feat: add speaker postgresql repository

---
+++ speaker/repository/postgresql/speaker_postgresql.go
@@ -0,0 +1,31 @@
+package postgresql
+
+import (
+	"speakerseeker-backend/domain"
+
+	"gorm.io/gorm"
+)
+
+type SpeakerRepository struct {
+	db *gorm.DB
+}
+
+func NewSpeakerRepository(db *gorm.DB) domain.SpekaerRepository {
+	return &SpeakerRepository{db}
+}
+
+func (r *SpeakerRepository) FindAll() ([]domain.Speaker, error) {
+	var speakers []domain.Speaker
+	if err := r.db.Find(&speakers).Error; err != nil {
+		return speakers, err
+	}
+	return speakers, nil
+}
+
+func (r *SpeakerRepository) FindOne(id uint) (domain.Speaker, error) {
+	var speaker domain.Speaker
+	if err := r.db.Where("id = ?", id).First(&speaker).Error; err != nil {
+		return speaker, err
+	}
+	return speaker, nil
+}
