0447c28b6ffb4d701f674249b54e42c7fd0d86df
kobulwidodo <fadhilhan01@gmail.com>
2022-05-17 22:15:11 +0700

    feat: add relation to speaker career

--- domain/speaker.go
+++ domain/speaker.go
@@ -4,19 +4,20 @@ import "gorm.io/gorm"
 
 type Speaker struct {
 	gorm.Model
-	Name            string         `json:"name"`
-	HeaderTitle     string         `json:"header_title"`
-	Header          string         `json:"header"`
-	About           string         `gorm:"text" json:"about"`
-	VirtualFeeStart int32          `json:"virtual_fee_start"`
-	VirtualFeeStop  int32          `json:"virtual_fee_stop"`
-	SiteFeeStart    int32          `json:"site_fee_start"`
-	SiteFeeStop     int32          `json:"site_fee_stop"`
-	Tiktok          string         `json:"tiktok"`
-	Instagram       string         `json:"instagram"`
-	Linkedin        string         `json:"linkedid"`
-	ImgPath         string         `json:"img_path"`
-	SpeakerSkills   []SpeakerSkill `json:"speaker_skills"`
+	Name            string          `json:"name"`
+	HeaderTitle     string          `json:"header_title"`
+	Header          string          `json:"header"`
+	About           string          `gorm:"text" json:"about"`
+	VirtualFeeStart int32           `json:"virtual_fee_start"`
+	VirtualFeeStop  int32           `json:"virtual_fee_stop"`
+	SiteFeeStart    int32           `json:"site_fee_start"`
+	SiteFeeStop     int32           `json:"site_fee_stop"`
+	Tiktok          string          `json:"tiktok"`
+	Instagram       string          `json:"instagram"`
+	Linkedin        string          `json:"linkedid"`
+	ImgPath         string          `json:"img_path"`
+	SpeakerSkills   []SpeakerSkill  `json:"speaker_skills"`
+	SpeakerCareers  []SpeakerCareer `json:"speaker_careers"`
 }
 
 type SpekaerRepository interface {
