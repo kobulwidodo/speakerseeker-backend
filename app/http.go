95a765e90277990cdf4cbee792109c12e33e44f7
kobulwidodo <fadhilhan01@gmail.com>
2022-05-17 23:22:21 +0700

    refactor: add ssl mode to env

--- app/http.go
+++ app/http.go
@@ -50,12 +50,13 @@ func main() {
 }
 
 func initDb() (*gorm.DB, error) {
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai",
+	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s TimeZone=Asia/Shanghai",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASS"),
 		os.Getenv("DB_NAME"),
 		os.Getenv("DB_PORT"),
+		os.Getenv("DB_SSLMODE"),
 	)
 
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
