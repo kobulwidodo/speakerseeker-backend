45ddaa0e774b36c0d5fa95c7b94be56c4ff89749
kobulwidodo <fadhilhan01@gmail.com>
2022-05-17 20:49:41 +0700

    feat: add speaker handler

---
+++ speaker/delivery/http/speaker_handler.go
@@ -0,0 +1,45 @@
+package http
+
+import (
+	"net/http"
+	"speakerseeker-backend/domain"
+	"speakerseeker-backend/utils"
+
+	"github.com/gin-gonic/gin"
+)
+
+type SpeakerHandler struct {
+	speakerUsecase domain.SpeakerUsecase
+}
+
+func NewSpeakerHandler(r *gin.RouterGroup, su domain.SpeakerUsecase) {
+	handler := &SpeakerHandler{speakerUsecase: su}
+	api := r.Group("/speakers")
+	{
+		api.GET("/", handler.GetAll)
+		api.GET("/:id", handler.GetById)
+	}
+}
+
+func (h *SpeakerHandler) GetAll(c *gin.Context) {
+	speakers, err := h.speakerUsecase.GetAll()
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, utils.NewFailResponse(err.Error()))
+		return
+	}
+	c.JSON(http.StatusOK, utils.NewSuccessResponse("successfully fetch all data", speakers))
+}
+
+func (h *SpeakerHandler) GetById(c *gin.Context) {
+	uri := new(domain.UriById)
+	if err := c.ShouldBindUri(&uri); err != nil {
+		c.JSON(http.StatusBadRequest, utils.NewFailResponse(err.Error()))
+		return
+	}
+	speaker, err := h.speakerUsecase.GetById(uri.Id)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, utils.NewFailResponse(err.Error()))
+		return
+	}
+	c.JSON(http.StatusOK, utils.NewSuccessResponse("successfully fetch data", speaker))
+}
