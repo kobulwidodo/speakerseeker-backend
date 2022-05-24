8dac8b82d3e39a9ea656936abee0869505c982b9
kobulwidodo <fadhilhan01@gmail.com>
2022-05-24 20:54:58 +0700

    feat: define model midtrans transaction

---
+++ domain/midtrans_transaction.go
@@ -0,0 +1,23 @@
+package domain
+
+import (
+	"gorm.io/datatypes"
+	"gorm.io/gorm"
+)
+
+type MidtransTransaction struct {
+	gorm.Model
+	MidtransId    string         `json:"midtrans_id"`
+	PaymentType   string         `json:"payment_type"`
+	Amount        int            `json:"amount"`
+	Status        string         `json:"status"`
+	PaymentData   datatypes.JSON `json:"payment_data"`
+	TransactionId uint           `json:"transaction_id"`
+}
+
+type MidtransTransactionRepository interface {
+	Create(midtransTrx MidtransTransaction) error
+}
+
+type MidtransTransactionUsecase interface {
+}
