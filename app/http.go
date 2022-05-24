29a31c13bce8b7e27d32a2df3b4fe22e43ee46b0
kobulwidodo <fadhilhan01@gmail.com>
2022-05-24 20:43:11 +0700

    feat: init transaction

--- app/http.go
+++ app/http.go
@@ -5,13 +5,18 @@ import (
 	"log"
 	"os"
 	"speakerseeker-backend/domain"
+	"speakerseeker-backend/infrastructure"
 	"speakerseeker-backend/middleware"
+	_midtransTransactionRepository "speakerseeker-backend/midtrans_transaction/repository/postgresql"
 	_speakerHttpHandler "speakerseeker-backend/speaker/delivery/http"
 	_speakerRepository "speakerseeker-backend/speaker/repository/postgresql"
 	_speakerUsecase "speakerseeker-backend/speaker/usecase"
 	_speakerSkillHttpHandler "speakerseeker-backend/speaker_skill/delivery/http"
 	_speakerSkillRepository "speakerseeker-backend/speaker_skill/repository/postgresql"
 	_speakerSkillUsecase "speakerseeker-backend/speaker_skill/usecase"
+	_transactionHttpHandler "speakerseeker-backend/transaction/delivery/http"
+	_transactionRepository "speakerseeker-backend/transaction/repository/postgresql"
+	_transactionUsecase "speakerseeker-backend/transaction/usecase"
 	_userHttpHandler "speakerseeker-backend/user/delivery/http"
 	_userRepository "speakerseeker-backend/user/repository"
 	_userUsecase "speakerseeker-backend/user/usecase"
@@ -44,18 +49,23 @@ func main() {
 	api.Static("/images", "./images")
 
 	jwtMiddleware := middleware.NewAuthMiddleware()
+	midtransDriver := infrastructure.NewMidtransDriver()
 
 	userRepository := _userRepository.NewUserRepository(db)
 	speakerRepository := _speakerRepository.NewSpeakerRepository(db)
 	speakerSkillRepository := _speakerSkillRepository.NewSpeakerSkillRepository(db)
+	transactionRepository := _transactionRepository.NewTransactionRepository(db)
+	midtransTransactionRepository := _midtransTransactionRepository.NewMidtransTransactionRepository(db)
 
 	userUseCase := _userUsecase.NewUserUseCase(userRepository)
 	speakerUseCase := _speakerUsecase.NewSpeakserUsecase(speakerRepository, speakerSkillRepository)
 	speakerSkillUseCase := _speakerSkillUsecase.NewSpeakerSkillUsecase(speakerSkillRepository)
+	transactionUsecase := _transactionUsecase.NewTransactionUsecase(transactionRepository, midtransTransactionRepository, speakerRepository, &midtransDriver)
 
 	_userHttpHandler.NewUserHandler(api, userUseCase, jwtMiddleware)
 	_speakerHttpHandler.NewSpeakerHandler(api, speakerUseCase)
 	_speakerSkillHttpHandler.NewSpeakerSkillHandler(api, speakerSkillUseCase)
+	_transactionHttpHandler.NewTransactionHandler(api, transactionUsecase, jwtMiddleware)
 
 	r.Run()
 }
@@ -75,7 +85,7 @@ func initDb() (*gorm.DB, error) {
 		return db, err
 	}
 
-	if err := db.AutoMigrate(&domain.User{}, &domain.Speaker{}, &domain.SpeakerSkill{}, &domain.SpeakerCareer{}, &domain.SpeakerExperience{}); err != nil {
+	if err := db.AutoMigrate(&domain.User{}, &domain.Speaker{}, &domain.SpeakerSkill{}, &domain.SpeakerCareer{}, &domain.SpeakerExperience{}, &domain.Transaction{}, &domain.MidtransTransaction{}); err != nil {
 		return nil, err
 	}
 
